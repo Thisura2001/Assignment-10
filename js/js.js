94dc46d4dcca2b81ddd5cae2d7b4a2181efb13e6
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-05-16 10:29:26 +0530

    add backspace

--- js/js.js
+++ js/js.js
@@ -11,3 +11,7 @@ function calculateResult() {
     var result = eval(expression);
     document.getElementById('display').value = result;
 }
+function backspace() {
+    var displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, -1);
+}
